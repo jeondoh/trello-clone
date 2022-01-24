e4225f0ee22eaf36da0463a5f344b318e6ead1e1
jundoh <jundo1302@gmail.com>
2022-01-24 19:44:11 +0900

    feat: make todo Boards to use react-beautiful-dnd

--- src/theme.ts
+++ src/theme.ts
@@ -2,9 +2,7 @@ import "styled-components";
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
-  borderColor: "white",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
